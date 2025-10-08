562e8515a84e125fd234eab876a1cd209a7f5fe2
excniesNIED <hxcn@cnies.org>
2025-10-08 12:59:03 +0800

    Update ToastContainer component to enhance z-index for improved visibility of toast notifications.

--- frontend/src/components/ToastContainer.vue
+++ frontend/src/components/ToastContainer.vue
@@ -1,5 +1,5 @@
 <template>
-  <div class="fixed bottom-4 right-4 z-50 space-y-2">
+  <div class="fixed bottom-4 right-4 z-[9999] space-y-2">
     <TransitionGroup name="toast">
       <div
         v-for="toast in toasts"
